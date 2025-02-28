48ca36d345e7bad33c267752a743d992f4b51eae
naechutimon <chutimon.naenae@gmail.com>
2025-02-28 15:25:39 +0700

    chunelab24/3

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 //Write function count() here.
+
+int count(int arr[], int size) {
+    vector<int> uniqueNumbers(arr, arr + size);
+    sort(uniqueNumbers.begin(), uniqueNumbers.end());
+    auto last = unique(uniqueNumbers.begin(), uniqueNumbers.end());
+    return distance(uniqueNumbers.begin(), last);
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
